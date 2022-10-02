93ddd4d3aa63d550729e9daf52106abe148de0bc
JCosmos <jcosmosmakes@gmail.com>
2022-10-02 18:13:09 +1100

    a

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { useReducer, useState, useEffect, useRef } from 'react';
 import './App.css';
 import Modal from './Modal';
 
+const saveqLib = (qLib) => {
+  console.log(qLib)
+  localStorage.setItem("qLibrary", JSON.stringify(qLib))
+
+
+}
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'addBank':
@@ -22,7 +29,27 @@ const reducer = (state, action) => {
 
       saveqLib(j)
       return { qLibrary: j }
+    case 'removeBank':
 
+      let x = state.qLibrary;
+      x.splice(action.removedInd, 1);
+      saveqLib(x);
+      return { qLibrary: x }
+    case 'changeBank':
+      let h = state.qLibrary;
+      h[action.bankIndex] = action.changedBank;
+
+      saveqLib(h)
+
+      return { qLibrary: h }
+    case 'removeQuote':
+      let k = state.qLibrary;
+      let r = k[action.bankNum].quotes
+      r.splice(action.removedIn, 1);
+
+      k[action.bankNum].quotes = r
+      saveqLib(k);
+      return { qLibrary: k }
 
     default:
       return state
@@ -38,11 +65,7 @@ const getLib = () => {
   }
 }
 
-const saveqLib = (qLib) => {
-  localStorage.setItem("qLibrary", JSON.stringify(qLib))
 
-
-}
 
 const useKeyPress = (target) => {
 
@@ -73,8 +96,13 @@ const mod = (n, m) => {
   return ((n % m) + m) % m;
 }
 function App() {
+  const [_, forceUpdate] = useReducer((x) => x + 1, 0);
   let x = getLib()
   const [open, setOpen] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  const [editOn, setEditOn] = useState(false);
+
 
   const control = useKeyPress("Control");
   const tab = useKeyPress("Tab");
@@ -83,9 +111,12 @@ function App() {
   const space = useKeyPress("Space")
 
   const currentQuoteLearnRef = useRef(null);
+  const rerenderth = useRef(null);
 
   const [currNumber, setCurrNumber] = useState(0);
   const [quoteShown, setQuoteShown] = useState(true);
+
+
 
   const [learnOn, setLearnOn] = useState(false);
   const [viewOn, setViewOn] = useState(false);
@@ -132,6 +163,15 @@ function App() {
 
   const [currQuoteLearn, setCurrQuoteLearn] = useState("");
 
+  const [tempEditQuotes, setTempEditQuotes] = useState([]);
+
+  const startEdit = () => {
+    // console.log(state.qLibrary[selected])
+    setEditOn(true);
+    setTempEditQuotes(state.qLibrary[selected].quotes);
+    // console.log(state.qLibrary[selected])
+  }
+
   const processLearn = (e, correctQuote) => {
     if (e.repeat) return;
     if (e.key == "Enter") {
@@ -141,7 +181,6 @@ function App() {
         currentQuoteLearnRef.current.value = "";
       } else {
         setCorrect(-1);
-        console.log("oi")
         dispatch({ type: 'addIncorrect', bankNum: selected, quoteInd: currNumber })
       }
     }
@@ -161,9 +200,24 @@ function App() {
     })
     // console.log({ name: tempName, quotes: b })
     dispatch({ type: "addBank", newBank: { name: tempName, quotes: b } });
+
     setTempQuotes(["Quote 1"]);
     setTempName("");
     setOpen(false);
+    location.href='/';
+    
+
+
+  }
+
+  const del = (selected) => {
+
+    if (prompt(`Type ${state.qLibrary[selected].name.toUpperCase()} to delete`) == state.qLibrary[selected].name.toUpperCase()) {
+      dispatch({ type: 'removeBank', removedInd: selected })
+      setSelected(-1);
+      setViewOn(false);
+    }
+
 
   }
 
@@ -172,16 +226,77 @@ function App() {
     q[i] = e.target.value;
     setTempQuotes(tempQuotes);
   }
+  const handleEditChange = (e, i) => {
+    let q = tempEditQuotes;
+    q[i].text = e.target.value;
+    setTempEditQuotes(q);
+    // let m = state.qLibrary[selected];
+    // console.log(m)
 
-  let r = 300;
-  // let qLibrary = [{ name: "1984.1", quotes: [] }, { name: "Tempest", quotes: [] }, { name: "JAP", quotes: [] },];
+  }
+
+  const resetLearn = () => {
+    setLearnOn(false);
+    setCurrNumber(0);
+    setCorrect(0);
+  }
+
+  const resetView = () => {
+    setEditOn(false);
+    setTempEditQuotes([]);
+  }
+  const saveChanges = () => {
+
+
+    let m = state.qLibrary[selected];
+    // console.log(m)
+    m.quotes.forEach((quote, index) => {
+      m.quotes[index].text = tempEditQuotes[index].text
+    })
+    // console.log(m)
+    dispatch({ type: "changeBank", bankIndex: selected, changedBank: m })
+    resetView()
+
+  }
+  const discardChanges = () => {
+    resetView()
+
+  }
+
+  const addQuoteEditing = () => {
+    let m = state.qLibrary[selected];
+    m.quotes.push({ incorrect: 0, correct: 0, text: "New Quote" })
+    dispatch({ type: "changeBank", bankIndex: selected, changedBank: m })
+  }
+  const resetProgress = (i) => {
+    if (confirm("Reset Progress?")) {
+      let m = state.qLibrary[selected]
+      m.quotes[i].correct = 0;
+      m.quotes[i].incorrect = 0;
+      dispatch({ type: "changeBank", bankIndex: selected, changedBank: m })
+    }
+
+  }
+  const deleteQuote = (i) => {
+    if (state.qLibrary[selected].quotes.length > 1) {
+      if (confirm("Delete quote?")) {
+        dispatch({ type: "removeQuote", bankNum: selected, removedInd: i })
+
+      }
+    } else {
+      alert("You must have at least one quote");
+    }
+
+  }
+
+  let r = 300 + (state.qLibrary.length > 8 ? 140 / (2 * Math.PI) * (state.qLibrary.length - 8) : 0);
   return (
     <>
       <div className="App">
         <div className='books'>
           {state.qLibrary.map((book, index) => {
 
-            return <div onClick={() => { if (selected !== index) setSelected(index); else setSelected(-1) }} style={{ position: 'fixed', transform: `translate(${r * Math.cos(2 * Math.PI * index / (state.qLibrary.length + 1))}px,${r * Math.sin(2 * Math.PI * index / (state.qLibrary.length + 1))}px)` }} className={`${index === selected ? 'selBook' : 'book'}`} key={book.name}>
+            return <div onClick={() => { if (selected !== index) setSelected(index); else setSelected(-1) }} style={{ position: 'fixed', transform: `translate(${r * Math.cos(2 * Math.PI * index / (state.qLibrary.length + 1))}px,${r * Math.sin(2 * Math.PI * index / (state.qLibrary.length + 1))}px)` }} className={`${reset == false ? (index === selected ? 'selBook' : 'book') : 'reset'}`} key={book.name}>
               {book.name}
             </div>
 
@@ -197,9 +312,10 @@ function App() {
             {/* <div className='action'>Revise</div> */}
           </div>
         </div>
+        <div className='hardReset' onClick={() => { if (prompt("Type 'HARD RESET' to confirm") == "HARD RESET")  {localStorage.clear(); location.href='/'}}}>HARD RESET</div>
         <Modal open={open} onClose={() => setOpen(false)}>
           <div className='quoteBank'>
-            <div className='f' onClick={() => setOpen(false) || setTempQuotes(["Quote 1"])}>Cancel</div>
+            <div className='back' onClick={() => setOpen(false) || setTempQuotes(["Quote 1"])}>Cancel</div>
             <div className='main'>
               <input onChange={((e) => setTempName(e.target.value))} type="text" className="in" placeholder="Name of Quotebank" spellCheck="false"></input>
               <div className='quoteBankQuotes'>
@@ -224,13 +340,45 @@ function App() {
               <div className='quotesV'>
 
                 {state.qLibrary[selected].quotes.map((quote, index) => {
-                  return <div key={index} className='quoteV'>{quote.text} <div className='numberV'>{index + 1}</div><div className='stats'><div className='correct'>Correct: {quote.correct}</div> <div className='incorrect'>Incorrect: {quote.incorrect}</div> </div></div>
+                  if (editOn) {
+                    return <div key={index} onChange={(e) => handleEditChange(e, index)} className='quoteV'>
+                      <input type="text" className="editQ" defaultValue={quote.text}></input>
+                      <div className='numberV'>{index + 1}</div>
+
+                      <div className='actionsEdit'>
+
+                        <div className='resetProgress' onClick={() => { resetProgress(index) }}>Reset Progress</div>
+                        <div className='deleteQuote' onClick={() => { deleteQuote(index) }}>Delete Quote</div>
+
+                      </div>
+
+                    </div>
+                  } else {
+                    return <div key={index} className='quoteV'><div>{quote.text}</div> <div className='numberV'>{index + 1}</div><div className='stats'><div className='correct'>Correct: {quote.correct}</div> <div className='incorrect'>Incorrect: {quote.incorrect}</div> </div></div>
+                  }
                 })}
               </div>
 
             </div>
 
-            <div className='back2' onClick={() => setViewOn(false)}>Back</div>
+            <div className='back2' onClick={() => { setViewOn(false); resetView(); }}>Back</div>
+            <div className='editGen'>
+              {
+                editOn ? <div className='saveSt'>
+                  <div className='save' onClick={saveChanges}>Save</div>
+                  <div className='discard' onClick={discardChanges}>Discard</div>
+                </div>
+                  : <div className='edit' onClick={startEdit}>Edit</div>
+
+
+
+              }
+              <div className='addQuoteEdit' onClick={addQuoteEditing}>
+                + Add Quote
+              </div>
+            </div>
+
+            <div className='delete' onClick={() => del(selected)}>Delete ALL</div>
           </div> : ''
           }
 
@@ -249,7 +397,7 @@ function App() {
               </div>
 
 
-              <div className='back2' onClick={() => setLearnOn(false)}>Back</div>
+              <div className='back2' onClick={() => resetLearn()}>Back</div>
               <div className='up' onClick={backwards}>↑</div>
               <div className='down' onClick={forwards}>↓</div>
             </div>
